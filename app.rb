f819c33829066ea41a0442195b59c6b1f81d5b16
milligramme <milligramme.cc@gmail.com>
2011-07-05 00:20:41 +0900

    it works

---
+++ app.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+class Zakuro
+  def timeline
+    t = Twitter.public_timeline
+  end
+end
+
+helpers do
+  def convert_hex(str)
+    an = []
+    str.split(//).each do |s|
+      an << format("%x",s.unpack("U*")[0])
+    end
+    an.join("").scan(/.../)
+  end
+end
+
+get '/' do
+  @tl = Zakuro.new.timeline
+  haml :index
+end
+
+get '/style.css' do
+  sass :style
+end
